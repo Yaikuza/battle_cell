6de507388943eaade331b55d197d0473a9e8279a
Yaikuza <sck.wasu@gmail.com>
2026-06-16 06:37:55 +0700

    chore: remove godot/ from tracking (moved to Yaikuza/few)

--- godot/data/UpgradeData.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name UpgradeData
-
-@export var id: String = ""
-@export var display_name: String = ""
-@export var description: String = ""
-@export var tags: Array = []
-@export var mods: Array = []
